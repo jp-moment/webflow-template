84ee8fb93ceb1478471eb582df64837e6d3bf2a9
Jean-Paul <jpmoise@gmail.com>
2025-05-20 11:07:41 -0400

    Finalisation du build JS pour test Webflow

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,12 @@
-"use strict";(()=>{window.Webflow||(window.Webflow=[]);window.Webflow.push(()=>{console.log("\u2705 Mon JavaScript fonctionne depuis Webflow !")});})();
+"use strict";
+(() => {
+  // bin/live-reload.js
+  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+
+  // src/index.ts
+  window.Webflow ||= [];
+  window.Webflow.push(() => {
+    console.log("\u2705 Mon JavaScript fonctionne depuis Webflow !");
+  });
+})();
+//# sourceMappingURL=index.js.map
